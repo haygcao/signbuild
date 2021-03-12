cb5d251bead1f5e01a0f7b31028d505699f1bae6
lunnlew <4288315+lunnlew@users.noreply.github.com>
2021-03-12 17:08:22 +0800

    up

--- decrypt.js
+++ decrypt.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { argv } = require('process');
 
 (async () => {
-    const enckey = (process.env.enckey || argv[3] || 'iwillencasmkey') + '';
+    const enckey = (process.env.enckey || argv[2] || 'iwillencasmkey') + '';
     let buf = Buffer.from(fs.readFileSync(path.join(__dirname, 'asm.tar.gz.enc'))).toString()
     let enc_string = cryptoUtil.CryptoJS_RC4_decrypt(buf, enckey)
     fs.writeFileSync(path.join(__dirname, 'asm.tar.gz.dec'), Buffer.from(enc_string, 'hex'))
--- encrypt.js
+++ encrypt.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { argv } = require('process');
 
 (async () => {
-    const enckey = (process.env.enckey || argv[3] || 'iwillencasmkey') + '';
+    const enckey = (process.env.enckey || argv[2] || 'iwillencasmkey') + '';
     let buf = Buffer.from(fs.readFileSync(path.join(__dirname, 'asm.tar.gz'))).toString('hex')
     let enc_string = cryptoUtil.CryptoJS_RC4_encrypt(buf, enckey)
     fs.writeFileSync(path.join(__dirname, 'asm.tar.gz.enc'), enc_string)
